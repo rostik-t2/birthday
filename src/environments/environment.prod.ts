0bccd336575c76e9d0464268a225c0a67cf9d417
Rostislav Terekhov <rterekhov@solit-clouds.ru>
2019-11-04 01:01:38 +0300

    Подарки Аделии на 1 год

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyATG34J1N6pIg36nfDjDKQs5IlSLowwDaA",
-    authDomain: "mybirthday1509.firebaseapp.com",
-    databaseURL: "https://mybirthday1509.firebaseio.com",
-    projectId: "mybirthday1509",
-    storageBucket: "mybirthday1509.appspot.com",
-    messagingSenderId: "723977534167"
+    apiKey: "AIzaSyBAgQy6yX3tfajD3lncurxNLYiSQ3kvKcE",
+    authDomain: "baby-gifts.firebaseapp.com",
+    databaseURL: "https://baby-gifts.firebaseio.com",
+    projectId: "baby-gifts",
+    storageBucket: "baby-gifts.appspot.com",
+    messagingSenderId: "852674338473"
   }
 };
